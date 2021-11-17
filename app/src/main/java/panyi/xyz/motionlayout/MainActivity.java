9834641dc2be797ba3f5ecd11301de1f723941bf
yixin.panyi <panyi@yixin.im>
2021-11-17 15:39:05 +0800

    Initial commit

---
+++ app/src/main/java/panyi/xyz/motionlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package panyi.xyz.motionlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
